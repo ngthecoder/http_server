413fc0c6b4e258367435d04b1a36bb40a0c529a5
ngthecoder <ngoto0208@gmail.com>
2024-05-16 22:26:53 +0900

    added simplest http server

--- app/main.go
+++ app/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	l, err := net.Listen("tcp", ":4221")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		return
+	}
+	defer l.Close()
+	fmt.Println("Listening on port 4221")
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting:", err.Error())
+		return
+	}
+	defer conn.Close()
+
+	contents := "Hello"
+	responce := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Length: %d\r\nContent-Type: text/plain\r\n\r\n%s", len(contents), contents)
+	conn.Write([]byte(responce))
 }
